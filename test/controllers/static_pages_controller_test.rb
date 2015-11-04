da83c9f69fd56bb767959fbae03347c58d754596
Rahul Gautam <rahulgautam297@gmail.com>
2015-11-04 02:45:44 +0000

    add sign up

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -19,4 +19,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
     assert_select "title" , "About | Ruby on Rails tutorial app"
   end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title" , "Contact | Ruby on Rails tutorial app"
+  end
 end
